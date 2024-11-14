b42f07b7aee5b00fdfb05fb2485de113cb05d92b
Aditya Jha <adityajha.swe@gmail.com>
2024-11-14 10:09:07 -0600

    Front End Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [datetime, setDatetime] = useState('');
+  const [description, setDescription] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<spaN>.00</spaN></h1>
+      <form>
+        <div className='basic'>
+          <input type = "text" 
+                value={name} 
+                onChange={ev=>setName(ev.target.value)} 
+                placeholder={'Ente the value'}/>
+          <input value ={datetime}
+                  onChange={ev=>setDatetime(ev.target.value)}
+                  type = "datetime-local"/>
+        </div>
+        <div className='description'>
+          <input type = "text" 
+                  value={description}
+                  onChange = {ev=>setDescription(ev.target.value)}
+                  placeholder={'Description'}/>  
+        </div>
+        <button type="submit">
+          Add New Transaction
+        </button>
+      </form>
+        
+        <div className='transactions'>
+          <div className='transaction'>
+            <div className='left'>
+              <div className='name'>New Samsung TV</div>
+              <div className='description'>It was time for new Tv</div>
+            </div>
+            <div className='right'>
+              <div className='price red'>-$500</div>
+              <div className='datetime'>2022-12-18 15:45</div>
+            </div>
+          </div>
+
+          <div className='transaction'>
+            <div className='left'>
+              <div className='name'>New Samsung Phone</div>
+              <div className='description'>It was time for new Tv</div>
+            </div>
+            <div className='right'>
+              <div className='price green'>+$500</div>
+              <div className='datetime'>2022-12-18 15:45</div>
+            </div>
+          </div>
+
+
+
+        </div>
+              
+      
+    </main>
   );
 }
 
